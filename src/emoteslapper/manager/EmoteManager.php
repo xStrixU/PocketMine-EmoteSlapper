70ccd8c09f52ed065b699ab06b20bcb1974de6bf
Flappy <60063027+UltraFlappy@users.noreply.github.com>
2021-06-22 05:57:37 +0700

    More emotes

--- src/emoteslapper/manager/EmoteManager.php
+++ src/emoteslapper/manager/EmoteManager.php
@@ -24,7 +24,22 @@ class EmoteManager {
 	 "sad_sigh" => "98a68056-e025-4c0f-a959-d6e330ccb5f5",
 	 "the_elytra" => "7393aa53-9145-4e66-b23b-ec86def6c6f2",
 	 "giddy" => "738497ce-539f-4e06-9a03-dc528506a468",
-	 "hooray" => "c4b5b251-24d3-43eb-9c05-46be246aeefb"
+	 "hooray" => "c4b5b251-24d3-43eb-9c05-46be246aeefb",
+         "feeling_sick" => "bb6f1764-2b0b-4a3a-adfd-3334627cdee4",
+         "thinking" => "20bcb500-af82-4c2f-9239-e78191c61375",
+         "rofl" => "6f82688e-e549-408c-946d-f8e99b91808d",
+         "groovin" => "d863b9cc-9f8c-498b-a8a3-7ebd542cb08e",
+         "ballerina" => "79452f7e-ffa0-470f-8283-f5063348471d",
+         "toothless" => "a12252fa-4ec8-42e0-a7d0-d44fbc90d753",
+         "ahh_choo" => "f9345ebb-4ba3-40e6-ad9b-6bfb10c92890",
+         "bored" => "7a314ecf-f94c-42c0-945f-76903c923808",
+         "playing_zombie" => "5d644007-3cdf-4246-b4ca-cfd7a4318a1c",
+         "offering" => "21e0054a-5bf4-468d-bfc4-fc4b49bd44ac",
+         "mediating" => "85957448-e7bb-4bb4-9182-510b4428e52c",
+         "shy_giggling" => "f1e18201-729d-472d-9e4f-5cdd7f6bba0c",
+         "underwater_dancing" => "05af18ca-920f-4232-83cb-133b2d913dd6",
+         "fake_death" => "efc2f0f5-af00-4d9e-a4b1-78f18d63be79",
+         "hand_stand" => "5dd129f9-cfc3-4fc1-b464-c66a03061545"
 	];
 	
 	public static function getEmoteId(string $emote) : ?string {
